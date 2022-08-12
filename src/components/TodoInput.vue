b01d16c7c1dfe10624ac81febf814ab60980b5ae
hyoju <agleor@naver.com>
2022-08-12 11:00:13 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            +
+        </span>
+    </div>
+</template>
+<script>
+export default {
+    data: function(){
+        return{
+            newTodoItem: ""
+        }
+    },
+    methods:{
+        addTodo: function(){
+            if(this.newTodoItem !==''){
+                var obj ={completed: false, item: this.newTodoItem};
+                // 저장하는 로직
+                localStorage.setItem(this.newTodoItem, JSON.stringify(obj)); //자바스크립트를 스트링으로 변환
+                this.clearInput();
+            }
+        },
+        clearInput: function(){
+            this.newTodoItem='';
+        }
+    }
+}
+</script>
+<style scoped>
+input.focus{
+    outline: none;
+}
+.inputBox{
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input{
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.attBtn{
+    color: white;
+    vertical-align: middle;
+}
+</style>
